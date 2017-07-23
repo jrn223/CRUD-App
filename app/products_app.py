7d8d3fa10800bd9ec279241d755a5e636a67fd23
jrn223 <jrn223@stern.nyu.edu>
2017-07-23 16:23:41 -0400

    Welcome message

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,8 @@
-print("HELLO")
+import csv
+
+products = []
+
+print("------------------------------")
+print("PRODUCTS APPLICATION")
+print("------------------------------")
+print("Welcome to the Products Application! Enjoy and have a great day!")
